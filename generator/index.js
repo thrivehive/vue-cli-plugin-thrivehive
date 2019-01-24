5932d88ad4aaad0c84c72b9dc8e3e1b06c506f58
Nick Ford <nickforddesign@gmail.com>
2019-01-24 11:40:48 -0500

    update generator

--- generator/index.js
+++ generator/index.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 
-const importStyles = '\nimport \'./scss/styles.scss\'';
+const importStyles = '\nimport \'./scss/styles.scss\';';
 
 module.exports = (api) => {
   api.extendPackage({
     dependencies: {
-      '@thrivehive/eslint-config-vue': '^1.0.2'
+      '@thrivehive/eslint-config-node': '^1.0.3',
+      '@thrivehive/eslint-config-vue': '^1.0.3'
     }
   });
   api.render('./template');
@@ -13,14 +14,14 @@ module.exports = (api) => {
     // inject to main.js
     const ext = api.hasPlugin('typescript') ? 'ts' : 'js';
     const mainPath = api.resolve(`./src/main.${ext}`);
-    // get content
-    let contentMain = fs.readFileSync(mainPath, { encoding: 'utf-8' });
-    const lines = contentMain.split(/\r?\n/g).reverse();
-    // inject import
-    const lastImportIndex = lines.findIndex(line => line.match(/^import/));
-    lines[lastImportIndex] += importStyles;
-    // modify app
-    contentMain = lines.reverse().join('\n');
-    fs.writeFileSync(mainPath, contentMain, { encoding: 'utf-8' });
+    const contentMain = fs.readFileSync(mainPath, { encoding: 'utf-8' });
+    if (!contentMain.includes(importStyles)) {
+      const lines = contentMain.split(/\r?\n/g).reverse();
+      // inject import 'styles.scss';
+      const lastImportIndex = lines.findIndex(line => line.match(/^import/));
+      lines[lastImportIndex] += importStyles;
+      const output = lines.reverse().join('\n');
+      fs.writeFileSync(mainPath, output, { encoding: 'utf-8' });
+    }
   });
 };
