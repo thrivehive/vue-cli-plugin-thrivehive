7298dac9aa4649ec320ac6ee6dc53ca00738e88b
Nick Ford <nickforddesign@gmail.com>
2019-01-24 16:14:30 -0500

    fix linting

--- generator/index.js
+++ generator/index.js
@@ -2,9 +2,9 @@ const { injectChanges, lint } = require('./utils');
 
 module.exports = (api) => {
   api.extendPackage({
-    dependencies: {
-      '@thrivehive/eslint-config-node': '^1.0.3',
-      '@thrivehive/eslint-config-vue': '^1.0.3'
+    devDependencies: {
+      '@thrivehive/eslint-config-node': '^1.0.4',
+      '@thrivehive/eslint-config-vue': '^1.0.4'
     }
   });
   api.render('./template');
