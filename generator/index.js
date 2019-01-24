61538a15ac99a23d27b891588325e08f0de29c93
Nick Ford <nickforddesign@gmail.com>
2019-01-24 14:42:22 -0500

    refactor and add linting step to complete hook

--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,4 @@
-const fs = require('fs');
-
-const importStyles = '\nimport \'./scss/styles.scss\';';
+const { injectChanges, lint } = require('./utils');
 
 module.exports = (api) => {
   api.extendPackage({
@@ -11,17 +9,7 @@ module.exports = (api) => {
   });
   api.render('./template');
   api.onCreateComplete(() => {
-    // inject to main.js
-    const ext = api.hasPlugin('typescript') ? 'ts' : 'js';
-    const mainPath = api.resolve(`./src/main.${ext}`);
-    const contentMain = fs.readFileSync(mainPath, { encoding: 'utf-8' });
-    if (!contentMain.includes(importStyles)) {
-      const lines = contentMain.split(/\r?\n/g).reverse();
-      // inject import 'styles.scss';
-      const lastImportIndex = lines.findIndex(line => line.match(/^import/));
-      lines[lastImportIndex] += importStyles;
-      const output = lines.reverse().join('\n');
-      fs.writeFileSync(mainPath, output, { encoding: 'utf-8' });
-    }
+    injectChanges(api);
+    lint(api);
   });
 };
