338a36befdef0cc94b7e328aa6ba87be2591345d
Nick Ford <nickforddesign@gmail.com>
2019-01-24 16:58:56 -0500

    fix vue config

--- generator/template/vue.config.js
+++ generator/template/vue.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
   lintOnSave: 'error',
   chainWebpack: (config) => {
-    config.entry('app')
-      .clear()
-      .add('./src/js/main.js');
     config.resolve.alias
       .delete('@')
       .set('@', path.resolve(__dirname, 'src'))
