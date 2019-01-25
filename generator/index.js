71dccd034b3adff3a1e90714333cf3f6ef9c4449
Nick Ford <nickforddesign@gmail.com>
2019-01-25 14:25:01 -0500

    lint entire project

--- generator/index.js
+++ generator/index.js
@@ -2,6 +2,9 @@ const { injectChanges, lint } = require('./utils');
 
 module.exports = (api) => {
   api.extendPackage({
+    scripts: {
+      line: 'vue-cli-service lint -- ./'
+    },
     devDependencies: {
       '@thrivehive/eslint-config-node': '^1.0.4',
       '@thrivehive/eslint-config-vue': '^1.0.4'
