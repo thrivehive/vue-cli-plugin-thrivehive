27fb23fc726f3f529ef80fc09b784b700e4c82a9
Nick Ford <nickforddesign@gmail.com>
2019-07-08 11:06:49 -0400

    remove defunct code injection

--- generator/index.js
+++ generator/index.js
@@ -1,24 +1,11 @@
 const {
   injectReadme,
   injectCode,
-  replaceCode,
   lint
 } = require('./utils');
 
 const mainChanges = `
 import '@/utils/setup'`;
-
-const storybookConfigChanges = `
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.css'
-import '@/utils/setup'
-
-Vue.use(Vuetify)
-
-addDecorator(() => ({
-  template: '<v-app><story/></v-app>'
-}))`;
 
 module.exports = (api) => {
   api.extendPackage({
@@ -27,7 +14,7 @@ module.exports = (api) => {
       lint: 'vue-cli-service lint -- ./'
     },
     dependencies: {
-      'vee-validate': '^2.1.7'
+      'vee-validate': '^2.2.12'
     },
     devDependencies: {
       '@thrivehive/eslint-config-node': '^1.2.3',
@@ -42,18 +29,6 @@ module.exports = (api) => {
   api.onCreateComplete(() => {
     injectReadme(api);
     injectCode(api, './src/main', mainChanges);
-
-    if (api.hasPlugin('storybook')) {
-      const storybookConfigPath = './config/storybook/config';
-      injectCode(api, storybookConfigPath, storybookConfigChanges);
-      replaceCode(
-        api,
-        storybookConfigPath,
-        `import { configure } from '@storybook/vue'`,
-        `import { configure, addDecorator } from '@storybook/vue'`
-      );
-    }
-
     lint(api);
   });
 };
